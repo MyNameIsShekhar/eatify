91f6f3c53b948eaebe7f09aa5539045e36347908
Shekhar <151825448+MyNameIsShekhar@users.noreply.github.com>
2024-03-17 21:40:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Card from './components/Card'
 import Category from './components/Category'
 import Food from './components/Food'
-import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 
 
